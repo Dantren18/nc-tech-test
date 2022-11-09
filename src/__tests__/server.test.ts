94d6d4cddc83899ac298e8cf23d15caa071dd7bd
Dantren18 <davidarenwick@gmail.com>
2022-11-09 16:12:53 +0000

    Get all cards endpoint now working and returning data in correct format

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,33 +1,30 @@
 import * as request from "supertest";
-import { app } from "../server";
 
-describe("GET /cards", () => {
-  test.only("Status 200: Returns an array of objects containing correct keys", async () => {
+const { app } = require("../server");
+
+describe.only("GET /cards", () => {
+  test("Status 200: Returns an array of objects containing correct keys", async () => {
     return request(app)
-        .get("/cards")
-        .expect(200)
-        .then(({ body }) => {
-          expect(Array.isArray(body.cards)).toEqual(true);
-          expect(body.cards[0]).toEqual(
-            expect.objectContaining({
-              title: "card 1 title",
-              card_id: "card001",
-              imageUrl: "/front-cover-portrait-1.jpg",
-            })
-          )
-          expect(body.cards.length).toEqual(3);
+      .get("/cards")
+      .expect(200)
+      .then(({ body }) => {
+        expect(Object.keys(body.cards[0])).toEqual([
+          "card_id",
+          "title",
+          "imageUrl"
+        ]);
+        expect(Array.isArray(body.cards)).toEqual(true);
+        expect(body.cards[0]).toEqual({
+          title: "card 1 title",
+          card_id: "card001",
+          imageUrl: "/front-cover-portrait-1.jpg"
         });
-    });
+        expect(body.cards.length).toEqual(3);
+      });
   });
-
-  test("Status 200: All objects have values attached to all keys", async () => {
-    const response = await request(app).get("/cards/");
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual(
-      expect.objectContaining({
-        title: "card 1 title"
-      })
-    );
+  test("Status 404: directory spelled incorrectly", async () => {
+    const response = await request(app).get("/carsd/");
+    expect(response.status).toBe(404);
   });
 });
 
--- src/card.model.js
+++ src/card.model.js
@@ -2,14 +2,17 @@ const cardData = require("./data/cards.json");
 const templateData = require("./data/templates.json");
 
 exports.getAllCardsModel = () => {
-  console.log("in the model");
   const formattedCardData = cardData.map((card) => {
+    const imageUrl = templateData.filter(
+      (data) => data.id == card.pages[0].templateId
+    );
     return {
       card_id: card.id,
       title: card.title,
-      template: card.pages[0].templateId
+      imageUrl: imageUrl[0].imageUrl
     };
   });
+
   return formattedCardData;
 };
 
