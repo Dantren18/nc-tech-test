b5ee01bda363bf572f75e3e9089c47bf974eadf3
Dantren18 <davidarenwick@gmail.com>
2022-11-09 14:01:41 +0000

    initial test setup

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,11 +1,36 @@
-import * as request from 'supertest'
-import { app } from '../server'
+import * as request from "supertest";
+import { app } from "../server";
 
-test('returns matching card title', async () => {
-  const response = await request(app).get('/cards/card001')
+describe("GET /cards", () => {
+  test.only("Status 200: Returns an array of objects containing correct keys", async () => {
+    const response = await request(app).get("/cards/");
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        title: "card 1 title"
+      })
+    );
+  });
+  test("Status 200: All objects have values attached to all keys", async () => {
+    const response = await request(app).get("/cards/");
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        title: "card 1 title"
+      })
+    );
+  });
+});
 
-  expect(response.status).toBe(200)
-  expect(response.body).toEqual(expect.objectContaining({
-    title: 'card 1 title',
-  }))
-})
+describe("GET /cards/:cardId", () => {
+  test("Status 200: Returns matching card title", async () => {
+    const response = await request(app).get("/cards/card001");
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual("test");
+  });
+  test("Status 404: card not found for ID that doesn't exist", async () => {
+    const response = await request(app).get("/cards/card9534853");
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual("test");
+  });
+});
